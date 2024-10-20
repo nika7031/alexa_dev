388f8f88b1d57c7c87b8740d27479d4204457ff0
nika7031 <158290453+nika7031@users.noreply.github.com>
2024-10-20 16:35:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react(),
   chunkSizeWarningLimit: 1000,
 });
